2e25b0a42319502d7b11b5de9b1ddb72a083d200
anslt <ge75wed@mytum.de>
2022-02-01 19:27:54 +0100

    modify code

--- scripts/train_panoptic_bev.py
+++ scripts/train_panoptic_bev.py
@@ -436,7 +436,10 @@ def train(model, optimizer, scheduler, dataloader, meters, **varargs):
 
         batch_time = time.time()
 
-        print(sample)
+        print("img:", sample["img"]._tensors.size())
+        print("bev_msk", sample["bev_msk"]._tensors.size())
+        print("front_msk", sample["front_msk"]._tensors.size())
+        print("weights_msk", sample["weights_msk"._tensors.size()])
 
         # Run network
         losses, results, stats = model(**sample, do_loss=True, do_prediction=False)
