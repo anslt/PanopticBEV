4723302e1c74e059e88ef752e21e2616a7006c99
anslt <ge75wed@mytum.de>
2022-02-01 18:02:50 +0100

    modify code

--- scripts/train_panoptic_bev_nuscenes_examples.sh
+++ scripts/train_panoptic_bev_nuscenes_examples.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 \
-python3 -m train_panoptic_bev.py \
+python3 train_panoptic_bev.py \
     --run_name=nuscenes_`date +"%Y_%m_%d_%H_%M_%S"` \
     --project_root_dir=/usr/stud/shil/storage/user/shil/PanopticBEV_mine/PanopticBEV \
     --seam_root_dir=/usr/stud/shil/storage/user/shil/PanopticBEV_mine/PanopticBEV/nuScenes_panopticbev_mini2 \
@@ -7,6 +7,6 @@ python3 -m train_panoptic_bev.py \
     --mode=train \
     --train_dataset=nuScenes \
     --val_dataset=nuScenes \
-    --config=nuscenes.ini \
+    --config=nuscenes_example.ini \
     --debug=True \
     --local_rank=0
