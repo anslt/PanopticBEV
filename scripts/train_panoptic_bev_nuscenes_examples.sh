ec2dfda011413be2426d225cddfdf9dbaaec45e8
anslt <ge75wed@mytum.de>
2022-02-01 17:40:46 +0100

    modify code

--- scripts/train_panoptic_bev_nuscenes_examples.sh
+++ scripts/train_panoptic_bev_nuscenes_examples.sh
@@ -1,9 +1,12 @@
-python -m script/train_panoptic_bev.py \
+CUDA_VISIBLE_DEVICES=0 \
+python3 -m train_panoptic_bev.py \
     --run_name=nuscenes_`date +"%Y_%m_%d_%H_%M_%S"` \
-    --project_root_dir="D:/MASTER/TUM/2021_2_fall/guided_research2/old_code/PanopticBEV" \
-    --seam_root_dir="D:/MASTER/TUM/2021_2_fall/guided_research2/data/nuScenes_panopticbev_mini2" \
-    --dataset_root_dir="D:/MASTER/TUM/2021_2_fall/guided_research2/data/nuScenes_panopticbev_mini2" \
+    --project_root_dir=/usr/stud/shil/storage/user/shil/PanopticBEV_mine/PanopticBEV \
+    --seam_root_dir=/usr/stud/shil/storage/user/shil/PanopticBEV_mine/PanopticBEV/nuScenes_panopticbev_mini2 \
+    --dataset_root_dir=/usr/stud/shil/storage/user/shil/PanopticBEV_mine/PanopticBEV/nuScenes_panopticbev_mini2 \
     --mode=train \
     --train_dataset=nuScenes \
     --val_dataset=nuScenes \
-    --config=nuscenes_example.ini
+    --config=nuscenes.ini \
+    --debug=True \
+    --local_rank=0
