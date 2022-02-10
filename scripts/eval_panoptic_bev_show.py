dccad166d8b97e787fb01ce43c0548550486d4ed
anslt <ge75wed@mytum.de>
2022-02-10 04:23:36 +0100

    modify the code

--- scripts/eval_panoptic_bev_show.py
+++ scripts/eval_panoptic_bev_show.py
@@ -140,10 +140,10 @@ def make_dataloader(args, config, rank, world_size):
 
     if args.test_dataset == "Kitti360":
         test_db = BEVKitti360Dataset(seam_root_dir=args.seam_root_dir, dataset_root_dir=args.dataset_root_dir,
-                                    split_name=dl_config['val_set'], transform=test_tf)
+                                    split_name=dl_config['show_set'], transform=test_tf)
     elif args.test_dataset == "nuScenes":
         test_db = BEVNuScenesDataset(seam_root_dir=args.seam_root_dir, dataset_root_dir=args.dataset_root_dir,
-                                    split_name=dl_config['val_set'], transform=test_tf)
+                                    split_name=dl_config['show_set'], transform=test_tf)
 
     if not args.debug:
         test_sampler = DistributedARBatchSampler(test_db, dl_config.getint("val_batch_size"), world_size, rank, False)
