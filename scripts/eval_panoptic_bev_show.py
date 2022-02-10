cbe22d8ccd5009849bca91ee50d07aa130c1f8c6
anslt <ge75wed@mytum.de>
2022-02-10 02:00:01 +0100

    modify the code

--- scripts/eval_panoptic_bev_show.py
+++ scripts/eval_panoptic_bev_show.py
@@ -445,7 +445,7 @@ def test(model, dataloader, **varargs):
             panoptic_pred_list = panoptic_post_processing(results, idxs, sample['bev_msk'], sample['cat'],
                                                           sample["iscrowd"])
 
-            imshow = visualise_bev(sample["img"], sample["bev_msk"], panoptic_pred_list,
+            imshow = visualise_bev([sample["img"][0]], [panoptic_pred_list[0]["seg_gt"],panoptic_pred_list[0]["cat_gt"]], [panoptic_pred_list[0]],
                                    num_stuff=num_stuff,
                                    rgb_mean=varargs["rgb_mean"],
                                    rgb_std=varargs["rgb_std"],
