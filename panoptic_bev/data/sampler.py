be72b70618043ceded6e0adc6b48b99a0c60e1c7
anslt <ge75wed@mytum.de>
2022-02-17 00:35:55 +0100

    add new sbatch

--- panoptic_bev/data/sampler.py
+++ panoptic_bev/data/sampler.py
@@ -6,12 +6,13 @@ from torch.utils.data.sampler import Sampler
 
 
 class ARBatchSampler(Sampler):
-    def __init__(self, data_source, batch_size, drop_last=False, epoch=0):
+    def __init__(self, data_source, batch_size, drop_last=False, epoch=0, is_train = True):
         super(ARBatchSampler, self).__init__(data_source)
         self.data_source = data_source
         self.batch_size = batch_size
         self.drop_last = drop_last
         self._epoch = epoch
+        self._is_train = is_train
 
         # Split images by orientation
         self.img_sets = self._split_images(range(len(data_source)))
@@ -29,12 +30,13 @@ class ARBatchSampler(Sampler):
         return img_sets
 
     def _generate_batches(self):
-        g = torch.Generator()
-        g.manual_seed(self._epoch)
+        if self._is_train:
+            g = torch.Generator()
+            g.manual_seed(self._epoch)
 
-        # Shuffle the two sets separately
-        self.img_sets[0] = [self.img_sets[0][i] for i in torch.randperm(len(self.img_sets[0]), generator=g)]
-        self.img_sets[1] = [self.img_sets[1][i] for i in torch.randperm(len(self.img_sets[1]), generator=g)]
+            # Shuffle the two sets separately
+            self.img_sets[0] = [self.img_sets[0][i] for i in torch.randperm(len(self.img_sets[0]), generator=g)]
+            self.img_sets[1] = [self.img_sets[1][i] for i in torch.randperm(len(self.img_sets[1]), generator=g)]
 
         batches = []
         leftover = []
@@ -78,8 +80,8 @@ class ARBatchSampler(Sampler):
 
 
 class DistributedARBatchSampler(ARBatchSampler):
-    def __init__(self, data_source, batch_size, num_replicas=None, rank=None, drop_last=False, epoch=0):
-        super(DistributedARBatchSampler, self).__init__(data_source, batch_size, drop_last, epoch)
+    def __init__(self, data_source, batch_size, num_replicas=None, rank=None, drop_last=False, epoch=0, is_train=True):
+        super(DistributedARBatchSampler, self).__init__(data_source, batch_size, drop_last, epoch, is_train)
 
         # Automatically get world size and rank if not provided
         if num_replicas is None:
@@ -99,9 +101,15 @@ class DistributedARBatchSampler(ARBatchSampler):
     def __iter__(self):
         batches = self._generate_batches()
 
-        g = torch.Generator()
-        g.manual_seed(self._epoch)
-        indices = list(torch.randperm(len(batches), generator=g))
+        if self._is_train is True:
+
+            g = torch.Generator()
+            g.manual_seed(self._epoch)
+            indices = list(torch.randperm(len(batches), generator=g))
+
+        else:
+
+            indices = list(range(len(batches)))
 
         # add extra samples to make it evenly divisible
         indices += indices[:(self.num_batches * self.num_replicas - len(indices))]
