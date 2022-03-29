54ae9c9d7bc0785c7112abc5cf339a143dd04c5d
anslt <slt1996@qq.com>
2022-03-29 10:30:56 +0200

    try

--- panoptic_bev/algos/fpn.py
+++ panoptic_bev/algos/fpn.py
@@ -359,14 +359,20 @@ class InstanceSegAlgoFPN(InstanceSegAlgo):
         # Sample rois
         rois = x[0].new_zeros(proposals.size(0), x[0].size(1), self.roi_size[0], self.roi_size[1])
         print(rois.shape)
-        if proposals.get_device() == 1:
-            torch.save(proposals.cpu(), "proposals.pt")
-            torch.save(proposals_idx.cpu(), "proposals_idx.pt")
+        # if proposals.get_device() == 1:
+        #    torch.save(proposals.cpu(), "proposals.pt")
+        #    torch.save(proposals_idx.cpu(), "proposals_idx.pt")
+        torch.load("proposals.pt")
+        torch.load("proposals_idx.pt")
+        proposals = proposals.cuda()
+        proposals_idx = proposals_idx.cuda()
         for level_i, x_i in enumerate(x):
             idx = target_level == (level_i + self.min_level)
             print("Traget Level ", level_i, ": ", torch.sum(idx))
-            if x_i.get_device() == 1:
-                torch.save(x_i.cpu(), "x_i_"+str(level_i)+".pt")
+            # if x_i.get_device() == 1:
+            #    torch.save(x_i.cpu(), "x_i_"+str(level_i)+".pt")
+            torch.load("x_i_"+str(level_i)+".pt")
+            x_i = x_i.cuda()
             if idx.any().item():
                 rois[idx] = self._rois(x_i, proposals[idx], proposals_idx[idx], img_size)
 
